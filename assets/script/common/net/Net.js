802f04f178dd2ca9abea10adf6068e7cb259dd01
liubowen <liubowen>
2018-04-30 18:09:19 +0800

    commit

--- assets/script/common/net/Net.js
+++ assets/script/common/net/Net.js
@@ -5,8 +5,34 @@ Object.defineProperty(exports, "__esModule", { value: true });
  */
 var Net = /** @class */ (function () {
     function Net(url) {
-        this.socketIO = io.connect(url);
+        this.socket = io.connect(url);
+        this.init();
     }
+    Net.prototype.init = function () {
+        this.socket.on("connect", this.connect);
+        this.socket.on("disconnect", this.disconnect);
+        this.socket.on("connect_failed", this.connect_failed);
+        this.socket.on("error", this.error);
+        this.socket.on("message", this.message);
+        this.socket.on("reconnect", this.reconnect);
+        this.socket.on("reconnect_failed", this.reconnect_failed);
+    };
+    Net.prototype.connect = function (data) {
+        this.isConnect = true;
+    };
+    Net.prototype.disconnect = function (data) {
+        this.isConnect = false;
+    };
+    Net.prototype.connect_failed = function (data) {
+    };
+    Net.prototype.error = function (data) {
+    };
+    Net.prototype.message = function (data) {
+    };
+    Net.prototype.reconnect = function (data) {
+    };
+    Net.prototype.reconnect_failed = function (data) {
+    };
     return Net;
 }());
 exports.Net = Net;
--- typescript/common/net/Net.ts
+++ typescript/common/net/Net.ts
@@ -3,10 +3,50 @@
  */
 export class Net {
 
-    private socketIO: Socket;
+    private socket: Socket;
+    private isConnect: boolean;
 
     constructor(url: string) {
-        this.socketIO = io.connect(url);
+        this.socket = io.connect(url);
+        this.init();
+    }
+
+    private init(): void {
+        this.socket.on("connect", this.connect);
+        this.socket.on("disconnect", this.disconnect);
+        this.socket.on("connect_failed", this.connect_failed);
+        this.socket.on("error", this.error);
+        this.socket.on("message", this.message);
+        this.socket.on("reconnect", this.reconnect);
+        this.socket.on("reconnect_failed", this.reconnect_failed);
+    }
+
+    private connect(data: any): void {
+        this.isConnect = true;
+    }
+
+    private disconnect(data: any): void {
+        this.isConnect = false;
+    }
+
+    private connect_failed(data: any): void {
+
+    }
+
+    private error(data: any): void {
+
+    }
+
+    private message(data: any): void {
+
+    }
+
+    private reconnect(data: any): void {
+
+    }
+
+    private reconnect_failed(data: any): void {
+
     }
 
 }
